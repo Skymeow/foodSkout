4cbf3c593f12b0b1abf2cec4000d1107d7acb583
Specialist17 <arocho.fernando.13@gmail.com>
2017-12-11 16:48:28 -0800

    started setting up user

---
+++ FoodSkoutVersion2/FoodSkout/Model/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  FoodSkout
+//
+//  Created by Fernando on 12/11/17.
+//  Copyright © 2017 Sky Xu. All rights reserved.
+//
+
+import Foundation
+
+struct User: Codable {
+    var username: String!
+    var email: String!
+}
